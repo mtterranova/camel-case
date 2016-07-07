ee07f344dcaceedc915b912ba20418783f19e5be
Max Einstein <maximillianeinstein@gmail.com>
2016-07-06 20:08:51 -0700

    [init] created skeleton

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: '.client/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx|\.js$/,
+        exclude: /(node_modules|server)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+
+};
