614fcdf9123f5ecc759e3a860ce6bdeb17a3c4fc
Dailen Spencer <dailenspencer@gmail.com>
2016-07-12 15:50:05 -0700

    [Feature] Add First Stage of Category Menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PulseGrid from './PulseGrid/PulseGrid';
 import Nav from './NavBar/Nav';
+import CategoryMenu from './CategoryMenu/CategoryMenu';
 
 export default class Home extends React.Component {
 
@@ -8,8 +9,9 @@ export default class Home extends React.Component {
 
     return (
       <div>
-        <Nav />
-		<PulseGrid/>
+      	<Nav />
+        <PulseGrid />
+        <CategoryMenu />
       </div>
     );
 
