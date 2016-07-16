1e93c67270b53b756e7eef316017fad7c4984ea0
Max Einstein <maximillianeinstein@gmail.com>
2016-07-15 21:42:08 -0700

    moved path module to routes

--- server/server.js
+++ server/server.js
@@ -8,10 +8,6 @@ var app = express();
 
 /* Serve all static files from public folder */
 app.use(express.static('./client'));
-app.get('*', function (request, response){
-  response.sendFile(path.resolve("client","index.html"))
-})
-
 
 /* Include routes */
 require('./routes/routes')(app);
